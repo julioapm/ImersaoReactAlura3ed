c61f171b18878919e146cb022207bbe1459e862d
Júlio Pereira Machado <julioapm@gmail.com>
2021-07-18 19:37:16 -0300

    Final aula5

--- aula5/alurakut/pages/index.js
+++ aula5/alurakut/pages/index.js
@@ -4,6 +4,8 @@ import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 
 function ProfileSidebar(props) {
   return (
@@ -36,8 +38,8 @@ function ProfileRelationsBox(props) {
   );
 }
 
-export default function Home() {
-  const usuarioAleatorio = 'julioapm';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
   
   const [comunidades, setComunidades] =  React.useState([]); //React hooks
   
@@ -168,4 +170,29 @@ export default function Home() {
     </MainGrid>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then(resposta => resposta.json());
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    };
+  }
+  const {githubUser} = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
